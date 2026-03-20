efbe80691305dbc66f1881a4e103d89f2cd8175e
Phạm Minh Đạt <gaaralbakuu@gmail.com>
2026-03-20 22:15:13 +0700

    Fix bug double ,

--- pkg/providers/github_copilot_provider.go
+++ pkg/providers/github_copilot_provider.go
@@ -41,7 +41,7 @@ func NewGitHubCopilotProvider(uri string, connectMode string, model string) (*Gi
 		}
 
 		session, err := client.CreateSession(context.Background(), &copilot.SessionConfig{
-			Model: model,,
+			Model: model,
 			OnPermissionRequest: copilot.PermissionHandler.ApproveAll,
 			Hooks: &copilot.SessionHooks{}
 		})
